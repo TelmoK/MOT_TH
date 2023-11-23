0da8f70f9060533540a3455e732d4dbd6198d888
Hugo Rocha <hugodbr@gmail.com>
2023-11-23 20:29:46 -0300

    update

--- Practica2/Assets/Scripts/CharacterMovement.cs
+++ Practica2/Assets/Scripts/CharacterMovement.cs
@@ -9,7 +9,18 @@ public class CharacterMovement : MonoBehaviour
     /// </summary>
     [SerializeField]
     private float _movementSpeed = 3.0f;
+    /// <summary>
+    /// Vertical speed assigned to character when jump starts
+    /// </summary>
+    [SerializeField]
+    private float _jumpSpeed = 20.0f;
+    /// <summary>
+    /// Minimum vertical speed to limitate falling speed
+    /// </summary>
+    [SerializeField]
+    private float _minSpeed = -10.0f;
     #endregion
+
     #region references
     /// <summary>
     /// Reference to Player's character controller
@@ -19,7 +30,12 @@ public class CharacterMovement : MonoBehaviour
     /// Reference to Player's Transform
     /// </summary>
     private Transform _myTransform;
+    /// <summary>
+    /// Reference to Camera's CameraController
+    /// </summary>
+    private CameraController _cameraController;
     #endregion
+
     #region properties
     /// <summary>
     /// Horizontal component of the movement direction
@@ -33,36 +49,72 @@ public class CharacterMovement : MonoBehaviour
     /// Movement direction vector
     /// </summary>
     private Vector3 _movementDirection;
+    /// <summary>
+    /// Movement vertical speed (needs to be updated every frame due to gravity)
+    /// </summary>
+    private float _verticalSpeed;
     #endregion
+
     #region methods
     /// <summary>
-    /// Public method to set the horizontal component of the movement direction
+    /// Public method to set the horizontal component of input. (Will be called from InputManager)
     /// </summary>
-    /// <param name="x">Desired horizontal component</param>
+    /// <param name="x">Received horizontal component</param>
     public void SetHorizontalInput(float x)
     {
         _xAxis = x;
     }
     /// <summary>
-    /// Public method to set the vertical component of the movement direction
+    /// Public method to set the vertical component of input. (Will be called from InputManager)
     /// </summary>
-    /// <param name="y">Desired vertical component</param>
+    /// <param name="y">Received vertical component</param>
     public void SetVerticalInput(float y)
     {
         _zAxis = y;
     }
+    /// <summary>
+    /// Public method called when the player tries to perform a new Jump. (Will be called from InputManager)
+    /// If the Character is grounded, it overrides current value or _verticalSpeed with _jumpSpeed.
+    /// Otherwise, the request to jump is ignored.
+    /// </summary>
+     public void Jump()
+     {
+        //TODO
+        Debug.Log("Jumped");
+     }
+
     #endregion
     /// <summary>
-    /// Used to initialize references
+    /// START
+    /// Needs to assign _myCharacterController, _myTransform and _cameraController.
+    /// If InputManager is already assigned, it will also register the player on it.
     /// </summary>
     void Start()
     {
+        // Programación defensiva
         _myCharacterController = GetComponent<CharacterController>();
+        if (_myCharacterController == null)
+        {
+            Debug.LogError("CharacterController not found on the GameObject.");
+        }
+
+        // Programación defensiva
+        _cameraController = FindObjectOfType<CameraController>();
+        if (_cameraController == null)
+        {
+            Debug.LogError("CameraController not found in the scene.");
+        }
+    
         _myTransform = transform;
     }
     /// <summary>
-    /// Used to set movement direction, apply speed to character controller
-    /// and set forward direction of the player
+    /// UPDATE
+    /// Needs to calculate and normalize horizontal movement direction
+    /// Needs to update vertical speed according to gravity
+    /// Finally move the character according to desired _movementSpeed in horizontal and updated _verticalSpeed
+    /// Final details:
+    /// -Ensure the character looks in the desired direction according to move direction.
+    /// -Ensure to set the vertical following behaviour for camera depending on whether the character is grounded or not.
     /// </summary>
     void Update()
     {
--- Practica2/Assets/Scripts/InputManager.cs
+++ Practica2/Assets/Scripts/InputManager.cs
@@ -1,33 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class InputManager : MonoBehaviour
 {
+    #region references
     /// <summary>
-    /// Reference to Player's CharacterMovement component, to be set from editor
+    /// Reference to Player's CharacterMovement component, to be set from editor first time,
+    /// and reassigned in run time for subsequent times.
     /// </summary>
     [SerializeField]
     private CharacterMovement _playerCharacterMovement;
+    #endregion
+
+    #region methods
     /// <summary>
-    /// Called every frame.
-    /// Needs to assign horizontal and vertical axis for Player's Character Movement.
-    ///
+    /// Public method to allow CharacterMovement to register on InputManager so it can receive input.
+    /// </summary>
+    /// <param name="playerCharacterMovement">Player's CharacterMovement (Component to be reigstered)</param>
+    public void RegisterPlayer(CharacterMovement playerCharacterMovement)
+    {
+        //TODO
+    }
+    #endregion
+
+    /// <summary>
+    /// UPDATE
+    /// Receive Horizontal input from player, if any, and set it on Player's CharacterMovement
+    /// Receive Vertical input from player, if any, and set it on Player's CharacterMovement
+    /// Receive Jump input from player, if any, and call corresponding method on Player's CharacterMovement
     /// </summary>
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            _playerCharacterMovement.SetHorizontalInput(1);
-        }
-        else if (Input.GetKey(KeyCode.A)) _playerCharacterMovement.SetHorizontalInput(-1);
-        else _playerCharacterMovement.SetHorizontalInput(0);
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    _playerCharacterMovement.SetHorizontalInput(1);
+        //}
+        //else if (Input.GetKey(KeyCode.A)) _playerCharacterMovement.SetHorizontalInput(-1);
+        //else _playerCharacterMovement.SetHorizontalInput(0);
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            _playerCharacterMovement.SetVerticalInput(1);
-        }
-        else if (Input.GetKey(KeyCode.S)) _playerCharacterMovement.SetVerticalInput(-1);
-        else _playerCharacterMovement.SetVerticalInput(0);
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    _playerCharacterMovement.SetVerticalInput(1);
+        //}
+        //else if (Input.GetKey(KeyCode.S)) _playerCharacterMovement.SetVerticalInput(-1);
+        //else _playerCharacterMovement.SetVerticalInput(0);
 
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        _playerCharacterMovement.SetHorizontalInput(horizontalInput);
+
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        _playerCharacterMovement.SetVerticalInput(verticalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _playerCharacterMovement.Jump();
+        }
     }
 }
